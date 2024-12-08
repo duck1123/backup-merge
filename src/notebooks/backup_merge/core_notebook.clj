16f35c223de39a758b1881bc8b9707029f84320e
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-12-08 13:59:07 -0500

    cheshire

--- src/notebooks/backup_merge/core_notebook.clj
+++ src/notebooks/backup_merge/core_notebook.clj
@@ -1,6 +1,8 @@
 (ns notebooks.backup-merge.core-notebook
   (:require
    [babashka.fs :as fs]
+   [cheshire.core :as json]
+   [clojure.java.io :as io]
    [nextjournal.clerk :as clerk]))
 
 ;; # Backup Merge
@@ -9,8 +11,7 @@
 
 (fs/cwd)
 
+(def backup-files (fs/list-dir data-path))
+
 (clerk/html
- [:ul (map
-       (fn [p]
-         [:li (str p)])
-       (fs/list-dir data-path))])
+ [:ul (map (fn [p] [:li (str p)]) backup-files)])
